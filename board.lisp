afdc06605bb2f595fe1b2e899ff932cef9dd2ad1
Mike Vollmer <mike@talkingincircles.net>
2013-09-16 12:23:02 -0700

    generating moves

--- board.lisp
+++ board.lisp
@@ -1,6 +1,8 @@
 ;;;; board.lisp
 
-(defconstant initial-positions
+(in-package #:lalla)
+
+(defparameter initial-positions
   #( 5  3  4  6  7  4  3  5
      0  0  0  0  0  0  0  0
      1  1  1  1  1  1  1  1
@@ -17,9 +19,9 @@
      0  0  0  0  0  0  0  0
     13 11 12 15 14 12 11 13
      0  0  0  0  0  0  0  0))
-(defparameter* (board (simple-array 128 (unsigned-byte 4)))
-    (make-array 128 :element-type (unsigned-byte 4) 
-		:initial-contents initial-positions))
+(defparameter board
+  (make-array 128 :element-type '(unsigned-byte 4) 
+	      :initial-contents initial-positions))
 (defun reset-board ()
   (loop for i from 0 to 127 do 
        (setf (aref board i) (aref initial-positions i))))
@@ -27,7 +29,6 @@
 (defparameter* (w-castle boolean) nil)
 (defparameter* (b-castle boolean) nil)
 
-(declare (inline off-board next-square get-piece square-color square-type blank-square))
 (defun* (off-board -> boolean) ((i (mod 128)))
   (/= (logand i #x88) 0))
 (defun* (next-square -> (mod 128)) ((i (mod 128)))
@@ -40,3 +41,4 @@
   (piece-type (get-piece i)))
 (defun* (blank-square -> boolean) ((i (mod 128)))
   (= 0 (square-type i)))
+(declaim (inline off-board next-square get-piece square-color square-type blank-square))
--- move.lisp
+++ move.lisp
@@ -4,7 +4,6 @@
 
 ;; Moves are stored in 16-bit words.
 ;; They consist of a from square, a to square, and a special tag.
-(declare (inline move-from move-to move-tag move-capture move-promotion make-move))
 (defun* (move-from -> (unsigned-byte 7)) ((m (unsigned-byte 16)))
   (ldb (byte 7 0) m))
 (defun* (move-to -> (unsigned-byte 7)) ((m (unsigned-byte 16)))
@@ -16,40 +15,48 @@
 (defun* (move-promotion -> boolean) ((m (unsigned-byte 16)))
   (= (ldb (byte 1 1) (move-tag m)) 1))
 (defun* (make-move -> (unsigned-byte 16))
-    ((from (unsigned-byte 6)) (to (unsigned-byte 6)) 
-     (tag (unsigned-byte 4)))
+    ((from (unsigned-byte 7)) (to (unsigned-byte 7)) 
+     (tag (unsigned-byte 2)))
   (let ((m 0))
     (declare ((unsigned-byte 16) m))
-    (setf (ldb (byte 6 0) m) from)
-    (setf (ldb (byte 6 6) m) to)
-    (setf (ldb (byte 4 12) m) tag)
+    (setf (ldb (byte 7 0) m) from)
+    (setf (ldb (byte 7 7) m) to)
+    (setf (ldb (byte 2 14) m) tag)
     m))
+(declaim (inline move-from move-to move-tag move-capture move-promotion make-move))
 
 ;; Generate moves using tables in piece.lisp
 (defconstant max-move-count 218)
-(defun* (generate-moves -> (vector (unsigned-byte 16))) ((square (mod 128)))
-  (*let ((piece (unsigned-byte 4) (aref board square))
-	 (color (unsigned-byte 1) (piece-color piece))
-	 (type (unsigned-byte 3) (piece-type piece))
-	 (sliding boolean (aref sliding-piece type))
-	 (start (unsigned-byte 8) (aref step-offset type))
-	 (step (unsigned-byte 8) (aref piece-steps start))
-	 (iter-square (mod 129) square)
-	 (moves-vector (vector (unsigned-byte 16))
-		       (make-array max-move-count :fill-pointer 0)))
-	(loop while (/= step 0) do
-	     (setf iter-square square)
-	     (loop while (< (+ iter-square step) 129) do ; inner loop for different step amounts
-		  (incf iter-square step) ; make one step
-		  (if (or (off-board iter-square) ; jump off board
-			  (and (not (blank-square iter-square)) ; non-blank square
-			       (= color (square-color iter-square)))) ; hit own piece
-		      (return) ; break	    
-		      (progn ; make move
-			(vector-push (make-move square iter-square 0) moves-vector) ; TODO: set move tag
-			(unless sliding (return))))) ; break if not sliding piece		   
-	     (incf start) ; try next step
-	     (setf step (aref piece-steps start)))
-	moves-vector)) ; return moves vector
+(defun* (generate-moves -> (vector (unsigned-byte 16))) ()
+  (let ((moves-vector 
+	 (make-array max-move-count
+		     :element-type '(unsigned-byte 16)
+		     :fill-pointer 0
+		     :initial-element 0)))
+    (loop 
+       for square from 0 to 127 
+       when (not (off-board square)) do
+	 (*let ((piece (unsigned-byte 4) (aref board square))
+		(color (unsigned-byte 1) (piece-color piece))
+		(type (unsigned-byte 3) (piece-type piece))
+		(sliding boolean (aref sliding-piece type))
+		(start (unsigned-byte 8) (aref step-offset type))
+		(step (signed-byte 8) (aref piece-steps start))
+		(iter-square (mod 128) square))
+	       (loop while (/= step 0) do
+		    (setf iter-square square)
+		    (loop while (and (> (+ iter-square step) -1)
+				     (< (+ iter-square step) 128)) do ; inner loop for different step amounts
+			 (incf iter-square step) ; make one step
+			 (if (or (off-board iter-square) ; jump off board
+				 (and (not (blank-square iter-square)) ; non-blank square
+				      (= color (square-color iter-square)))) ; hit own piece
+			     (return) ; break	    
+			     (progn ; make move
+			       (vector-push (make-move square iter-square 0) moves-vector) ; TODO: set move tag
+			       (unless sliding (return))))) ; break if not sliding piece		   
+		    (incf start) ; try next step
+		    (setf step (aref piece-steps start)))))
+    moves-vector))
 		    
 		    
